d726cc78cc2db999b6ac02cdd58da7c031b7d837
manulis <manulisdesable@gmail.com>
2025-03-16 12:01:17 +0100

    [UP] terraform config

---
+++ terraform/resource_group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "name" {
+  name = "rg-cp2"
+  location = "West Europe"
+}
---
+++ terraform/varibles.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+    description = "Azure Subsciption ID"
+    type = string
+}
+
+variable "tenant_id" {
+    description = "Azure Tenant ID"
+    type = string
+}
