0ddcd8a7073e886f0bd9ba6788eba3eb2a1a72ca
manulis <manulisdesable@gmail.com>
2025-03-16 12:21:46 +0100

    [UP] terraform azure VM

--- terraform/resource_group.tf
+++ terraform/resource_group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "name" {
+resource "azurerm_resource_group" "rg" {
   name = "rg-cp2"
   location = "West Europe"
 }
---
+++ terraform/vm.tf
@@ -0,0 +1,89 @@
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "VNet-CP2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "Subnet-CP2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "NIC-CP2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "NSG-CP2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "AllowSSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "PublicIP-CP2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_ssh_public_key" "ssh_key" {
+  name                = "AzureSSHKey-CP2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  public_key          = file(".ssh/id_rsa.pub")
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "UbuntuVM-CP2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = azurerm_ssh_public_key.ssh_key.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
